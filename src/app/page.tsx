a2022de335f833ed49ea1b5fbfbaf80b94cecee3
rimever <mot_siva_ordin@hotmail.com>
2025-04-11 08:47:00 +0900

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+<div className="min-h-screen w-full flex items-center justify-center">
+  <main>
+    <h1 className="text-5xl font-bold">Hello World!</h1>
+  </main>
+</div>
+  );
+}
